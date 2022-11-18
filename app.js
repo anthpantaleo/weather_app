4c4b78df0cf8b99bb969d9e4225623ba563dd636
anthpantaleo <anthpantaleo@gmail.com>
2022-11-18 17:51:21 -0500

    added some func

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const cityInput = document.querySelector("#city");
 const errorDisplay = document.querySelector(".errordisplay");
 const tempDisplay = document.querySelector("#temp");
+const feelsLikeDisplay = document.querySelector("#feelslike");
 const windDisplay = document.querySelector("#wind");
+const descriptionDisplay = document.querySelector("#something");
+const cityTitle = document.querySelector("#citytitle");
 
-const API_KEY = "dca67048a8605acd92961e5186a52be3";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=";
 
-// errorDisplay.style.display = "block";
+const API_KEY = "&appid=dca67048a8605acd92961e5186a52be3";
+const API_UNITS = "&units=metric";
 
 cityInput.addEventListener("change", (e) => {
   e.preventDefault();
 
   searchedCity = cityInput.value.toLowerCase();
   cityInput.value = "";
-  console.log(searchedCity);
+
+  getWeather(searchedCity);
 });
+
+async function getWeather(searchedCity) {
+  const searchURL = url + searchedCity + API_KEY + API_UNITS;
+
+  errorDisplay.style.display = "none";
+  const response = await fetch(searchURL);
+  const results = await response.json();
+
+  if (response.status === 404) {
+    errorDisplay.style.display = "block";
+  } else {
+    updateWeatherDisplay(results);
+  }
+}
+
+function updateWeatherDisplay(results) {
+  console.log(results);
+}
