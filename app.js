0989d82a7e5456c3abce864a57a4203bcafe1a77
anthpantaleo <anthpantaleo@gmail.com>
2022-11-18 17:21:18 -0500

    added selectors and some information holders

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const cityInput = document.querySelector("#city");
 const errorDisplay = document.querySelector(".errordisplay");
-//   errorDisplay.style.display = "block";
+const tempDisplay = document.querySelector("#temp");
+const windDisplay = document.querySelector("#wind");
+
+const API_KEY = "dca67048a8605acd92961e5186a52be3";
+
+// errorDisplay.style.display = "block";
 
 cityInput.addEventListener("change", (e) => {
   e.preventDefault();
 
   searchedCity = cityInput.value.toLowerCase();
   cityInput.value = "";
+  console.log(searchedCity);
 });
